e3dbcd7ba8bcc7186c993efc358d12e20d20aa6b
Ian <ianmesquita486@gmail.com>
2024-09-10 20:19:58 -0300

    fundamentos whilw

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// hello world
-let mensagem = "oi"
-mensagem = "saudades"
-const men2 = "oi vida!"
-const men3 = "te amo meu amor"
-console.log(mensagem);
-console.log(men2)
-console.log(men3)
+const start = () => {
+    let count = 0
+        while(count <= 10){
+            console.log(count)  
+            count = count + 1
+        } 
+}
+
+start()
