5e58831236fff8aa56af1fa031d6627537d6037b
Ian <ianmesquita486@gmail.com>
2024-09-10 22:46:16 -0300

    Menu de aplicação

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
-const start = () => {
+const { select } = require('@inquirer/prompts')
+
+const start = async() => {
 
     while(true){
-        let opcao = "sair"
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                     name: "cadastrar meta",
+                     value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                     name: "Sair",
+                     value: "sair"
+                }       
+            ]
+        })
+
+
+
         switch(opcao) {
-            case "cadastrar":
+            case "Cadastrar":
                 console.log("vamos cadastrar")
                 break
             case "listar":
                 console.log("vamos listar")
                 break
             case "sair":
+                console.log("Até a próxima!")
                 return        
         }
     }
